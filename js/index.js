4d70ff5a8244c2533ef84272482b2159c215a14c
Vinicius Callegari <callegari@hotmail.com>
2021-05-31 01:55:08 -0300

    criando um janela modal e adicionando funçoes a cada card de filme para exibir seu  respectivo trailer.

---
+++ js/index.js
@@ -0,0 +1,64 @@
+function abrirModalMini1(){ 
+        document.getElementById("open_modal").style.top = "0";
+        document.getElementById("video").innerHTML = '<iframe width="853" height="480" src="https://www.youtube.com/embed/5_4SW8HHfUs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+};
+function abrirModalMini2(){ 
+    document.getElementById("open_modal").style.top = "0";
+    document.getElementById("video").innerHTML = '<iframe width="876" height="365" src="https://www.youtube.com/embed/W0squnw6Jp8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+};
+function abrirModalMini3(){ 
+    document.getElementById("open_modal").style.top = "0";
+    document.getElementById("video").innerHTML = '<iframe width="853" height="480" src="https://www.youtube.com/embed/Ud-PtULy4gs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+};
+function abrirModalMini4(){ 
+    document.getElementById("open_modal").style.top = "0";
+    document.getElementById("video").innerHTML = '<iframe width="853" height="480" src="https://www.youtube.com/embed/FRLYZn5PGuA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+};
+function abrirModalMini5(){ 
+    document.getElementById("open_modal").style.top = "0";
+    document.getElementById("video").innerHTML = '<iframe width="876" height="367" src="https://www.youtube.com/embed/VWKYlN-fAWM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+};
+function abrirModalMini6(){ 
+    document.getElementById("open_modal").style.top = "0";
+    document.getElementById("video").innerHTML = '<iframe width="853" height="480" src="https://www.youtube.com/embed/FH_Pm-0kuFc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+};
+function abrirModalMini7(){ 
+    document.getElementById("open_modal").style.top = "0";
+    document.getElementById("video").innerHTML = '<iframe width="853" height="480" src="https://www.youtube.com/embed/dVlQJO4HZj0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+};
+function abrirModalMini8(){ 
+    document.getElementById("open_modal").style.top = "0";
+    document.getElementById("video").innerHTML = '<iframe width="853" height="480" src="https://www.youtube.com/embed/apltEQy8RzQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+};
+function abrirModalMini9(){ 
+    document.getElementById("open_modal").style.top = "0";
+    document.getElementById("video").innerHTML = '<iframe width="720" height="480" src="https://www.youtube.com/embed/yBHfuph4WWY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+};
+function abrirModalMini10(){ 
+    document.getElementById("open_modal").style.top = "0";
+    document.getElementById("video").innerHTML = '<iframe width="876" height="364" src="https://www.youtube.com/embed/t_ULBP6V9bg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+};
+function abrirModalHouseOfCards(){
+    document.getElementById("open_modal").style.top="0";
+    document.getElementById("video").innerHTML = '<iframe width="853" height="480" src="https://www.youtube.com/embed/SvSkxBYuoQY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+}
+function HouseOfCards(){
+    document.getElementById("open_modal").style.top="0",color="white";
+    // document.getElementById("titulo").innerHTML = '<h2>Resumo</h2>';
+    document.getElementById("video").innerHTML = '<p>O congressista Francis Underwood e sua mulher, Claire, fazem de tudo para conquistar seus objetivos, não importa o que aconteça. Um mundo político recheado de ganância, corrupção e luxúria na capital Washington.</p>'
+}
+function fecharModal(){
+    document.getElementById("open_modal").style.top = "-100%";
+};
+
+
